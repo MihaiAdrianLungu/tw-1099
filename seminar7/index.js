51c3253675258d5fccd8e703bfd8d991e3fc089b
Lungu Mihai Adrian <adrian.lungu@csie.ase.ro>
2024-11-13 17:55:12 +0200

    seminar 7

--- seminar7/index.js
+++ seminar7/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./user/user.routes');
+const dotenv = require('dotenv');
+const path = require('path');
 const app = express();
+dotenv.config();
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3001;
+ 
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(cors({
     origin: 'http://127.0.0.1:5500'
 }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/users', usersRoutes);
 
 app.get('/', function (req, res) {
--- seminar7/user/user.routes.js
+++ seminar7/user/user.routes.js
@@ -3,7 +3,49 @@ const users = require('./users');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json(users);
+    // const name = req.query.name;
+    // const age = req.query.age;
+    const { name, age } = req.query;
+    let filteredUsers = [...users];
+
+    if (name) {
+        filteredUsers = filteredUsers.filter(user => user.name.toLowerCase().includes(name.toLowerCase()))
+    }
+
+    if (age && !isNaN(age)) {
+        filteredUsers = filteredUsers.filter(user => user.age == Number(age));
+    }
+
+    res.status(200).json(filteredUsers);
+})
+
+router.get('/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+
+        if (isNaN(id)) {
+            throw new Error('not a number');
+        }
+
+        const user = users.find(user => user.id === Number(id));
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ message: 'User not found' })
+    }
+})
+
+router.post('/', (req, res) => {
+    const body = req.body;
+
+    const newUser = {
+        id: users.length + 1,
+        ...body,
+    }
+
+    users.push(newUser);
+
+    res.status(201).json(newUser);
 })
 
 module.exports = router;
